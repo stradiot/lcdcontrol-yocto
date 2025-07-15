4a771c3d7d5c7d923573bc2678d92845bc0dbc02
Martin Stradiot <martin.stradiot@gmail.com>
2025-07-15 23:06:22 +0000

    fix: console parameters

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Ensure you are in the project root (where .git and poky are)
-# before sourcing oe-init-build-env.
 cd /home/mstradiot/Documents/lcdcontrol-yocto/
 
-# Source the oe-init-build-env script, telling it to use ./conf as the build directory.
 source poky/oe-init-build-env ./build
