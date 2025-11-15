75f21f29c99d51df6096d7d7a60bec7f5811c670
Martin Stradiot <martin.stradiot@gmail.com>
2025-11-15 15:58:26 +0000

    fix: symlink the yocto conf files

--- setup-env.sh
+++ setup-env.sh
@@ -2,3 +2,11 @@
 cd /home/mstradiot/Documents/lcdcontrol-yocto/
 
 source poky/oe-init-build-env ./build
+
+# Remove the default, auto-generated files
+rm conf/local.conf
+rm conf/bblayers.conf
+
+# Create symlinks to your version-controlled files
+ln -s ../../yocto-conf/local.conf conf/local.conf
+ln -s ../../yocto-conf/bblayers.conf conf/bblayers.conf
